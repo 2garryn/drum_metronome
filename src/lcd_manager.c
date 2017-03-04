dffb13334a2e266ead89698f5f6cd89b457441c4
Artem Golovinsky <artemgolovinsky@gmail.com>
2017-03-04 07:27:39 +0300

    Display works and print some symbols

--- src/lcd_manager.c
+++ src/lcd_manager.c
@@ -13,29 +13,43 @@ static void lcd_sck(uint8_t state);
 static void lcd_led(uint8_t state);
 static void lcd_bit(uint16_t pin, uint8_t state);
 
+static void print_string(uint8_t dot[]);
+
 void lcd_demo() {
-	uint8_t dot[] = { 0x00, 0x06, 0x49, 0x49, 0x29, 0x1E };
-	uint8_t line;
+	uint8_t dotP[] = { 0x00, 0x7F, 0x09, 0x09, 0x09, 0x06 };
+    uint8_t dotI[] = { 0x00, 0x00, 0x41, 0x7F, 0x41, 0x00 };
+    uint8_t dotR[] = { 0x00, 0x7F, 0x09, 0x19, 0x29, 0x46 };
+    uint8_t dotO[] = { 0x00, 0x3E, 0x41, 0x41, 0x41, 0x3E };
+    uint8_t dotJ[] = { 0x00, 0x20, 0x40, 0x41, 0x3F, 0x01 };
+    uint8_t dotK[] = { 0x00, 0x7F, 0x08, 0x14, 0x22, 0x41 };
+
 
 	// X = 0 and Y = 0;
 	write_command(0x40);
 	write_command(0x80);
-
-	for (line = 0; line < 6; line++) {
-		write_data(dot[line]);
-	}
+    print_string(dotP);
+    print_string(dotI);
+    print_string(dotR);
+    print_string(dotO);
+    print_string(dotJ);
+    print_string(dotO);
+    print_string(dotK);
 }
 
+static void print_string(uint8_t dot[]) {
+  	for (uint8_t line = 0; line < 6; line++) {
+		write_data(dot[line]);
+	}  
+}
 
 void lcd_init() {
 	GPIO_InitTypeDef ports;
+    RCC_AHB1PeriphClockCmd(RCC_GPIO, ENABLE);
 	//Declare pins to configure
 	ports.GPIO_Pin = CS_PIN | RST_PIN | DC_PIN | DIN_PIN | SCK_PIN | LED_PIN;
 	ports.GPIO_Speed = GPIO_Speed_100MHz;
 	ports.GPIO_Mode = GPIO_Mode_OUT;
 	ports.GPIO_OType = GPIO_OType_PP;
-	//Start clock to the selected port
-	RCC_AHB1PeriphClockCmd(RCC_GPIO, ENABLE);
 	//Init Port
 	GPIO_Init(GPIO_PORT, &ports);
 	init_display();
@@ -45,20 +59,20 @@ void lcd_init() {
 static void init_display() {
 	lcd_led(RESET);
 	lcd_rst(RESET);
-	lcd_dc(SET); //command mode
-	lcd_din(SET);
-	lcd_sck(SET);
-	lcd_cs(SET);
 
 	systick_delay(10);
 	lcd_rst(SET);
 	write_command(0x21); //Extended instruction set selected
-	write_command(0xB8); //Set LCD voltage // 0xB7 from code
-	write_command(0x13); //Set Bias for 1/48 /// 0x14 from code
-	write_command(0x06); //Set temperature control (TC2)
+	write_command(0xC0); //Set LCD voltage // 0xB7 from code
+    write_command(0x06); //Set temperature control (TC2)
+	write_command(0x14); //Set Bias for 1/48 /// 0x14 from code
+
+    
 	write_command(0x20); //Revert to standard instruction set
+    write_command(0x0C);
+
 	clear_lcd();
-	write_command(0x0C);
+	
 }
 
 
